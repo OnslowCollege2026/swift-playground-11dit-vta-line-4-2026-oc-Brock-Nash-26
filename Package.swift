ce7ebd84ea70b25f6440cba408c09f6dfe1f448d
oc-Brock-Nash-26 <brock.nash@student.onslow.school.nz>
2026-06-18 23:48:52 +0000

    Jun 18, 2026, 11:48 PM

--- Package.swift
+++ Package.swift
@@ -13,3 +13,82 @@ let package = Package(
         ),
     ]
 )
+
+// The strings I will be using for gathering info on what the student wants.
+var drinks: [String] = [
+    "Juice - $4.00",
+    "Up & Go - $4.50",
+    "Gatorade - $6.00",
+    "Water - $3.00",
+    "Coffee - $6.00",
+    "Fruit soda drink - $4.00"
+
+]
+
+var foods: [String] = [
+    "Burger - $7.00",
+    "Sushi - $4.00",
+    "Pizza - $5.00",
+    "Garlic bread - $4.00",
+    "Pork bun - $5.00",
+    "Dumplings - $5.00",
+
+
+]
+
+var foodCounter = [""]
+
+var drinkCounter = [""]
+
+print("Welcome to the canteen. Please follow the instructions below")
+
+
+
+var isRunning = true
+while isRunning {
+    // Asking for user input.
+        print("Do you wish to buy a food or a drink:")
+        let foodOrDrink = (readLine()!)
+
+// Outcome dependant on user input.
+if foodOrDrink.lowercased() == "Food" {
+print("what food do you want, 1-6?")
+foods.enumerated().forEach { index, foods in
+    print("\(index + 1). \(foods)")
+    }
+        let whatFood = (readLine()!)
+        let food = foods[Int(whatFood)! - 1]
+        foodCounter.append(food)
+        print("\(food) has been added")
+
+// Telling the loop to keep going.
+    isRunning = true
+
+} else if foodOrDrink.lowercased() == "drink" {
+print("what drink do you want, 1-6?")
+drinks.enumerated().forEach { index, drinks in
+    print("\(index + 1). \(drinks)")
+    }
+        let whatDrink = (readLine()!)
+        let drink = drinks[Int(whatDrink)! - 1]
+        drinkCounter.append(drink)
+        print("\(drink) has been added")
+
+// Telling the loop to keep going.
+    isRunning = true
+
+} else if foodOrDrink == "" {
+        isRunning = false
+
+} else {
+    // If the input is unexpected
+    print("Please clarify what you wish to do, if you wish to select a drink or food, please enter 'drink' or 'food'")
+    print("Otherwise if you are done and wish to see your final price please press enter")
+
+        isRunning = true
+
+}
+}
+
+print(foodCounter)
+print(drinkCounter)
